ead6cbfc5ad0d6c96c800757ee9a8e083f582a20
unknown <diallomouhamed609@gmail.com>
2026-04-21 18:23:45 +0000

    deuxieme commit

--- backend/server.js
+++ backend/server.js
@@ -61,8 +61,8 @@ app.get('/', (req, res) => {
 const startServer = async () => {
   await connectDB();
   await createDefaultAdmin();
-  app.listen(PORT, () => {
-    console.log(`Backend running on http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Backend running on http://0.0.0.0:${PORT}`);
   });
 };
 
